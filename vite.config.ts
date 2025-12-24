d9b0716734667ae01e23558e953941ca7d4cf2fa
HyIsNoob <90138555+HyIsNoob@users.noreply.github.com>
2025-12-24 13:01:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default defineConfig(({ mode }) => {
